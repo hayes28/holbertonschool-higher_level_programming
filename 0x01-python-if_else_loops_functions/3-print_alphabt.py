632b3a2d0dbab61fc40363e1599a7eeae468bfdf
heather hayes <heather.hayes918@gmail.com>
2023-01-09 14:00:43 -0800

    added format

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 
-for letters in range(ord('a'), ord('z')+1):
-    letters = chr(letters)
-    if letters not in "qe":
-        print(letters, end="")
+for ltr in range(97, 123):
+    print("{}".format(chr(ltr)), end="")
