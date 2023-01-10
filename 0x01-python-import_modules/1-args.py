d88d1eaf6525bda52b43dfdf57bd7aff778604f7
heather hayes <heather.hayes918@gmail.com>
2023-01-09 16:39:14 -0800

    arg

---
+++ 0x01-python-import_modules/1-args.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import sys
+
+if __name__ == '__main__':
+    print("0 arguments.")
+else:
+    if (len(sys.arg) - 1) == 1:
+        print((len(sys.argv) -1), "argument:")
+    else:
+        print((len(sys.argv) - 1), "argument:")
+    for i in range(1, len(sys.argv)):
+        print("{}: {}".format(i, sys.argv[i]))
