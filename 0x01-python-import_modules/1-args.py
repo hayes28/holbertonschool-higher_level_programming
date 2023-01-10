6c1ea95c29be9a133e0547427e9c1176377566ca
heather hayes <heather.hayes918@gmail.com>
2023-01-09 16:43:52 -0800

    fixed with extra if statement

--- 0x01-python-import_modules/1-args.py
+++ 0x01-python-import_modules/1-args.py
@@ -2,11 +2,13 @@
 import sys
 
 if __name__ == '__main__':
-    print("0 arguments.")
-else:
-    if (len(sys.arg) - 1) == 1:
-        print((len(sys.argv) -1), "argument:")
+    
+    if (len(sys.argv) - 1) == 0:
+        print("0 arguments.")
     else:
-        print((len(sys.argv) - 1), "argument:")
-    for i in range(1, len(sys.argv)):
-        print("{}: {}".format(i, sys.argv[i]))
+        if (len(sys.arg) - 1) == 1:
+            print((len(sys.argv) -1), "argument:")
+        else:
+            print((len(sys.argv) - 1), "argument:")
+        for i in range(1, len(sys.argv)):
+            print("{}: {}".format(i, sys.argv[i]))
