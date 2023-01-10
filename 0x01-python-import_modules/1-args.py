1a0ac40b7b59a0bee0ce498d62527fdf59dfe870
heather hayes <heather.hayes918@gmail.com>
2023-01-09 17:04:03 -0800

    argv

--- 0x01-python-import_modules/1-args.py
+++ 0x01-python-import_modules/1-args.py
@@ -1,14 +1,12 @@
 #!/usr/bin/python3
-import sys
-
-if __name__ == '__main__':
-    
-    if (len(sys.argv) - 1) == 0:
-        print("0 arguments.")
-    else:
-        if (len(sys.arg) - 1) == 1:
-            print((len(sys.argv) -1), "argument:")
-        else:
-            print((len(sys.argv) - 1), "argument:")
-        for i in range(1, len(sys.argv)):
-            print("{}: {}".format(i, sys.argv[i]))
+from sys import argv
+__name__ == "__main__"
+if len(argv) == 1:
+    print("0 arguments.")
+elif len(argv) == 2:
+    print("1 argument:")
+    print("1: {}".format(argv[1]))
+else:
+    print("{} arguments:".format(len(argv) - 1))
+    for x in range(1, len(argv)):
+        print("{}: {}".format(x, argv[x]))
