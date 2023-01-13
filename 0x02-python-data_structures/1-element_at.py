b90ff1c884ef2eb2b5ec773c87c0e9bf217b2afb
HH <heather.hayes918@gmail.com>
2023-01-13 15:02:30 -0600

    trying with 'not in'

--- 0x02-python-data_structures/1-element_at.py
+++ 0x02-python-data_structures/1-element_at.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    if idx < 0 or idx >= range(len(my_list)):
+    if idx < 0 or idx not in range(len(my_list)):
         return None
     return my_list[idx]
