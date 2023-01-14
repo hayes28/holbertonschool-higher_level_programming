7f486cb75d6f4ba80828da0aba9dce3efb36b63a
HH <heather.hayes918@gmail.com>
2023-01-13 18:47:12 -0600

    Still getting the hang of vscode

--- 0x02-python-data_structures/5-no_c.py
+++ 0x02-python-data_structures/5-no_c.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    new_string = my_string.translate({ord('c'): ""})
-    new_string = my_string.translate({ord('C'): ""})
+    new_string = my_string.translate({ord(x): None for x in 'cC'})
     return new_string
