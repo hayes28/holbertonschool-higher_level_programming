80ac5fc6ee3d7c53b8054717d4098bb64e4fa6a6
HH <heather.hayes918@gmail.com>
2023-01-15 17:13:49 -0600

    Print sorted dict

---
+++ 0x03-python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def print_sorted_dictionary(a_dictionary):
+    # It prints the key-value pairs of the dictionary in a sorted order
+    # Using list comprehension and the sorted() function to sort the keys
+    # then it prints the key-value pairs using the format() method
+    [print("{}: {}".format(i, a_dictionary[i])) for i in sorted(a_dictionary)]
