90d6c5a58f4673292735d2f586b2438cfe741bd6
HH <heather.hayes918@gmail.com>
2023-01-15 15:59:13 -0600

    Combine sets

---
+++ 0x03-python-more_data_structures/4-only_diff_elements.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def only_diff_elements(set_1, set_2):
+    # This function takes two sets as parameters
+    # and returns a one set without duplicates
+
+    # Using the union operator '|' to combine elements
+    # from set_1 and set_2
+    return set_1 | set_2
