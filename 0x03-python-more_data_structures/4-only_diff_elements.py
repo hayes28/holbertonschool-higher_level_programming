1e825ee3812d4a54e3529753b85ade080a7df176
HH <heather.hayes918@gmail.com>
2023-01-15 16:12:02 -0600

    Changed to ^ from |

--- 0x03-python-more_data_structures/4-only_diff_elements.py
+++ 0x03-python-more_data_structures/4-only_diff_elements.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
     # This function takes two sets as parameters
-    # and returns a one set without duplicates
+    # and returns one set without duplicates
 
-    # Using the union operator '|' to combine elements
+    # Using the symmetric_difference() method '^' to combine elements
     # from set_1 and set_2
-    return set_1 | set_2
+    return set_1 ^ set_2
