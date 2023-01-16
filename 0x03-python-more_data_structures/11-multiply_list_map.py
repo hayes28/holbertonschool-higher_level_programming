9be4bbd3d01db4c67390dff30a5dd931376d6623
HH <heather.hayes918@gmail.com>
2023-01-15 18:45:14 -0600

    Cannot have comments, only allowed 3 lines

--- 0x03-python-more_data_structures/11-multiply_list_map.py
+++ 0x03-python-more_data_structures/11-multiply_list_map.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
 def multiply_list_map(my_list=[], number=0):
-    # Uses built-in 'map' function to apply the 'lambda' function
-    # to each element of the list
-    # 'lambda' multiplies each element of the list by the number parameter
-    # Converts the map object to a list, returns modified list
     return list(map((lambda x: x * number), my_list))
