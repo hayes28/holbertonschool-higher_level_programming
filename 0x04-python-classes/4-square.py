b9d6ea2fda6460623d0e3b7a92e1b334ca14afca
HH <heather.hayes918@gmail.com>
2023-01-17 16:38:12 -0600

    Added better comments

--- 0x04-python-classes/4-square.py
+++ 0x04-python-classes/4-square.py
@@ -1,23 +1,39 @@
 #!/usr/bin/python3
-""" Square class """
+"""
+Defines a square class with a single attribute, 'size', which is
+set during instantiation.
+"""
 
 
 class Square:
-    """defines size with value of 0"""
+    """
+    Initializes the square object with a size attribute
+    """
     def __init__(self, size=0):
         self.__size = size
 
-    """function to get the area of the square"""
+    """
+    Function to calculate the area of the square using the size
+    attribute.
+    """
     def area(self):
         return self.__size * self.__size
 
-    """getter and setter functions below"""
+    """
+    Getter and setter functions for the size attribute
+    """
     @property
     def size(self):
+        """
+        Getter function to retrieve the value of a size attribute
+        """
         return(self.__size)
 
     @size.setter
     def size(self, value):
+        """
+        Setter function to set the value of a size attribute
+        """
         if type(value) is not int:
             raise TypeError("size must be an integer")
         if value < 0:
@@ -25,6 +41,9 @@ class Square:
         self.__size = value
 
     def my_print(self):
+        """
+        Prints a square using '#' character
+        """
         if self.size > 0:
             for i in range(self.__size):
                 for j in range(self.__size):
