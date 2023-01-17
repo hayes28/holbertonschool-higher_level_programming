83dab4e9c74cfd4b5b17463af59606f973743ba1
HH <heather.hayes918@gmail.com>
2023-01-16 21:12:20 -0600

    init size of square

---
+++ 0x04-python-classes/0-square.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""" This is a square class """
+
+class Square:
+    """ Square class that initializes private attribute 'size'"""
+    def __init__(self, size):
+        self.__size = size
