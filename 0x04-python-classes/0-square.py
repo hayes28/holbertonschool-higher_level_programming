8c56bd679bb006b3cb544bae0704886a4caa693f
HH <heather.hayes918@gmail.com>
2023-01-16 21:17:15 -0600

    Didn't realized I needed two blank spaces b4 class

--- 0x04-python-classes/0-square.py
+++ 0x04-python-classes/0-square.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
-""" This is a square class """
+"This is a square class"
+
 
 class Square:
-    """ Square class that initializes private attribute 'size'"""
+
+    "Square class that initializes private attribute 'size'"
     def __init__(self, size):
         self.__size = size
