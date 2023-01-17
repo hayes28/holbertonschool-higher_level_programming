50d5a2e7a855cbb971e22c14325f124236698d84
HH <heather.hayes918@gmail.com>
2023-01-17 16:11:04 -0600

    getter setter

---
+++ 0x04-python-classes/3-square.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+" Square class "
+
+
+class Square:
+    "defines size with value of 0"
+    def __init__(self, size=0):
+        "user input needs to be a number and greater than or equal to 0"
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    " function to get the area of the square "
+    def area(self):
+        return self.__size * self.__size
+
+    "getter and setter functions below"
+    @property
+    def size(self):
+        return(self.__size)
+
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+    self.__size = value
