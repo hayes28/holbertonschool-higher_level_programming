a07ae5cc3ca3b2284c12862acb98a73f5f27e0f9
HH <heather.hayes918@gmail.com>
2023-01-17 16:19:13 -0600

    line 25 needed to be indented

--- 0x04-python-classes/3-square.py
+++ 0x04-python-classes/3-square.py
@@ -1,22 +1,17 @@
 #!/usr/bin/python3
-" Square class "
+""" Square class """
 
 
 class Square:
-    "defines size with value of 0"
+    """defines size with value of 0"""
     def __init__(self, size=0):
-        "user input needs to be a number and greater than or equal to 0"
-        if type(size) is not int:
-            raise TypeError("size must be an integer")
-        if size < 0:
-            raise ValueError("size must be >= 0")
         self.__size = size
 
-    " function to get the area of the square "
+    """function to get the area of the square"""
     def area(self):
         return self.__size * self.__size
 
-    "getter and setter functions below"
+    """getter and setter functions below"""
     @property
     def size(self):
         return(self.__size)
@@ -27,4 +22,4 @@ class Square:
             raise TypeError("size must be an integer")
         if value < 0:
             raise ValueError("size must be >= 0")
-    self.__size = value
+        self.__size = value
