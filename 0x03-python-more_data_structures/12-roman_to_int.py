82f3cabd214079f03b6dea5f8603a04d20afc229
HH <heather.hayes918@gmail.com>
2023-01-18 20:33:04 -0600

    Roman to int

--- 0x03-python-more_data_structures/12-roman_to_int.py
+++ 0x03-python-more_data_structures/12-roman_to_int.py
@@ -1,24 +1,32 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if type(roman_string) is not str or roman_string is None:
-            return
+    """
+    This function takes in a string as an argument and
+    converts it to a Roman numeral.
+    """
+    # check if input is not a string or is empty
+    if type(roman_string) != str or not roman_string:
+        return 0
 
-    length = len(roman_string)
-    romstr = roman_string
-    number = 0
-    numbers = {
-            "I": 1,
-            "V": 5,
-            "X": 10,
-            "L": 50,
-            "C": 100,
-            "D": 500,
-            "M": 1000,
-        }
-    for i in range(length):
-        if i + 1 < length and numbers[romstr[i]] < numbers[romstr[i + 1]]:
-            number -= numbers[romstr[i]]
+    # Initialize variables to store the final integer
+    # value and the previous value
+    count = 0
+    last = 0
+
+    # Create a dictionary of Roman numerals and
+    # their corresponding integer values
+    romNum = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
+              'C': 100, 'D': 500, 'M': 1000}
+
+    # Iterate through the input string
+    for x in roman_string:
+
+        # check if the last value is less than the current value
+        if last != 0 and last < romNum[x]:
+            count -= last
         else:
-            number += numbers[romstr[i]]
-        return number
-    return (0)
+            count += last
+        last = romNum[x]
+        # add the last value
+    count += last
+    return count
