a17db1ee4c9468ac1b9bc47c09da16ab76aa9138
HH <heather.hayes918@gmail.com>
2023-01-23 16:28:25 -0600

    Trying again but my output is printing backwards

--- 0x06-python-more_classes/0-rectangle.py
+++ 0x06-python-more_classes/0-rectangle.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 """
 Defines two private instance attributes 'width' and 'height'.
+TypeError will raise if width and height are not integers
+ValueError will raise if width and height are less than zero
+width and height are private attributes
 """
 
 
@@ -11,23 +14,35 @@ class Rectangle:
 
     @property
     def width(self):
+        '''
+        Retrieves width of self.width
+        '''
         return self.__width
+    
+    @property
+    def height(self):
+        '''
+        Retrieves height of self.height
+        '''
+        return self.__height
 
     @width.setter
     def width(self, value):
-        if not isinstance(value, int):
+        '''
+        Sets width of self rectangle
+        '''
+        if type(value) is not int:
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
 
-    @property
-    def height(self):
-        return self.__height
-
     @height.setter
     def height(self, value):
-        if not isinstance(value, int):
+        '''
+        Sets height of self rectangle
+        '''
+        if type(value) is not int:
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
