ddc572823613aa582ad439ada4b0bd3fbfe9950d
HH <heather.hayes918@gmail.com>
2023-01-23 16:37:30 -0600

    Right output now

--- 0x06-python-more_classes/0-rectangle.py
+++ 0x06-python-more_classes/0-rectangle.py
@@ -9,28 +9,28 @@ width and height are private attributes
 
 class Rectangle:
     def __init__(self, width=0, height=0):
-            self.width = width
-            self.height = height
+        self.height = height
+        self.width = width
 
     @property
     def width(self):
-        '''
+        """
         Retrieves width of self.width
-        '''
+        """
         return self.__width
     
     @property
     def height(self):
-        '''
+        """
         Retrieves height of self.height
-        '''
+        """
         return self.__height
 
     @width.setter
     def width(self, value):
-        '''
+        """
         Sets width of self rectangle
-        '''
+        """
         if type(value) is not int:
             raise TypeError("width must be an integer")
         if value < 0:
@@ -39,9 +39,9 @@ class Rectangle:
 
     @height.setter
     def height(self, value):
-        '''
+        """
         Sets height of self rectangle
-        '''
+        """
         if type(value) is not int:
             raise TypeError("height must be an integer")
         if value < 0:
