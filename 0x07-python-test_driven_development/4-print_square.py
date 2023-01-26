f56ee8873e046e6f273ec1f1cfb284fe0177e3a1
HH <heather.hayes918@gmail.com>
2023-01-25 21:38:41 -0600

    Print square

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Prints "Prints square"
+"""
+
+
+def print_square(size):
+    """
+    Checks to make sure the size is an int > 0,
+    then prints a square
+    """
+    if type(size) is not int:
+        raise TypeError("size must be an int > 0")
+    if type(size) is float and size < 0:
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    for i in range(size):
+        for j in range(size):
+            print("#", end="")
+        print()
