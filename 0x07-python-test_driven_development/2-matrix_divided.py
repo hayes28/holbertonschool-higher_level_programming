c83b48a8cb4b19a5f88523cee055d100893711cd
HH <heather.hayes918@gmail.com>
2023-01-25 21:19:30 -0600

    pep8 fix

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -2,9 +2,12 @@
 """
     Divide all elements of a matrix
 """
+
+
 type_error = "matrix must be a matrix (list of lists) of integers/floats"
 size_error = "Each row of the matrix must have the same size"
 num_error = "div must be a number"
+
 
 def matrix_divided(matrix, div):
     """_summary_ Divide all elements of a matrix with a div
@@ -18,7 +21,7 @@ def matrix_divided(matrix, div):
     size = len(matrix[0])
 
     for row in matrix:
-        if len(row)!= size:
+        if len(row) != size:
             raise TypeError(size_error)
         for i in row:
             if type(i) is not int and type(i) is not float:
