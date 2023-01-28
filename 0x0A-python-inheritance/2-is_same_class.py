bd13abb1ddedb015516aafc87755911560f36e51
HH <heather.hayes918@gmail.com>
2023-01-27 19:34:13 -0600

    2-Same

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Exact same object- returns True if the same, otherwise False
+"""
+
+
+def is_same_class(obj, a_class):
+    """
+    Checks if obj1 and obj2 are the exact same instances
+    """
+    if type(obj) == a_class:
+        return True
+    else:
+        return False
