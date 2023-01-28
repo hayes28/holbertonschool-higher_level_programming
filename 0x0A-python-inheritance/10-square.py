c687c657efdeb0526be4031035df8cd393cd647f
HH <heather.hayes918@gmail.com>
2023-01-28 14:02:27 -0600

    square child

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" Create class Square that inherits from Rectangle """
+
+
+Rectangle = __import__("9-rectangle").Rectangle
+
+
+class Square(Rectangle):
+    """ Square specific class """
+
+    def __init__(self, size):
+        """ initializes Square """
+
+        self.integer_validator("size", size)
+
+        self.__size = size
+
+    def area(self):
+        """ Returns area of the Square """
+
+        return(self.__size * self.__size)
+
+    def __str__(self):
+        """Returns the human readable
+        string representation of self, says [Rectanlge] """
+
+        return("[Rectangle] {}/{}".format(self.__size, self.__size))
