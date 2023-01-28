530fb7699b997fa6ada57cf92d0d02fdd34c7bf2
HH <heather.hayes918@gmail.com>
2023-01-28 13:24:26 -0600

    Base_Geo

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" Public instances for class BaseGeometry """
+
+
+class BaseGeometry:
+    """ Empty class """
+    
+    def area(self):
+        """ returns the area of a BaseGeometry
+        But using an Exception. The area is not implemented """
+    
+        raise Exception('area() is not implemented')
+
+    def integer_validator(self, name, value):
+        """ checks if the value is a valid integer """
+    
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
