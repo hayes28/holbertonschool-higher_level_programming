b92c037a4c6722c8a875b9681f391712553f2952
HH <heather.hayes918@gmail.com>
2023-01-27 19:05:12 -0600

    Lookup and README

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,11 @@
+#!/bin/python3
+"""
+Lookup attributes and methods of an object
+"""
+
+
+def lookup(obj):
+    """
+    Returns: a list of available attributes and methods of an object
+    """
+    return dir(obj)
