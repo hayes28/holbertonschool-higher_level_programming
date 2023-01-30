3a6f3175671bf121e5f66d5d8ff4c104978f6805
HH <heather.hayes918@gmail.com>
2023-01-30 14:54:49 -0600

    trying to understand testing

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -9,11 +9,6 @@ class MyList that inherits from list
 class MyList(list):
     """ Inherits from list """
     
-    def __inti__(self):
-        """ initializes MyList sub class, brings all attributes from list """
-        
-        super().__init__()
-        
     def print_sorted(self):
         """ Prints sorted list (ascending) """
         
