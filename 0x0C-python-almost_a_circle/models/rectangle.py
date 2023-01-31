8ac9b8f4fc6129197464ebcfd1abfc616f934b01
HH <heather.hayes918@gmail.com>
2023-01-31 17:19:16 -0600

    made public

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -8,7 +8,7 @@ class Rectangle(Base):
     def __init__(self, width, height, x=0, y=0, id=None):
         """Rectangle constructor"""
         super().__init__(id)
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
         self.x = x
         self.y = y
