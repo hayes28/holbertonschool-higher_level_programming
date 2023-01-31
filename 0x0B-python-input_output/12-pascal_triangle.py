60fc778860cb96bd63dd930a373d3744a823d97a
HH <heather.hayes918@gmail.com>
2023-01-30 19:19:46 -0600

    Pascal Triangle

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Pascal's Triangle-
+Technical interview preparation: """
+
+
+def pascal_triangle(n):
+    """ Returns list of Pascal's Triangle """
+    triangle = []
+    for i in range(n):
+        triangle.append([])
+        for j in range(i + 1):
+            if j == i or j == 0:
+                triangle[i].append(1)
+            else:
+                triangle[i].append(triangle[i - 1][j - 1] + triangle[i - 1][j])
+    return triangle
