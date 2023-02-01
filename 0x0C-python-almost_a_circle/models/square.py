7100732e2036a6cc0dab942b66f66128a25e3e42
HH <heather.hayes918@gmail.com>
2023-01-31 19:41:24 -0600

    changed super() self into size

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""Square Module"""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """Class Square"""
+    def __init__(self, size, x=0, y=0, id=None):
+        """Initialization of Square"""
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """Return string representation of Rectangle"""
+        return '[' + type(self).__name__ + '] (' + str(self.id) \
+            + ') ' + str(self.x) + '/' + str(self.y) + ' - ' \
+            + str(self.size)
+
+    @property
+    def size(self):
+        """Getter for size"""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """Setter for size"""
+        if type(value) is not int:
+            raise TypeError('size must be an integer')
+        if value <= 0:
+            raise ValueError('size must be > 0')
+        self.width = value
+        self.height = value
