e79ed91b288d8ca20aa0ee52a262927f18fb62a9
HH <heather.hayes918@gmail.com>
2023-02-10 22:27:55 -0600

    only SELECT privilege

--- 0x01-SQL_more_queries/2-create_read_user.sql
+++ 0x01-SQL_more_queries/2-create_read_user.sql
@@ -1,4 +1,5 @@
--- Create the MySQL server user 'user_0d_2'
+-- Create the database 'hbtn_0d_2' and the MySQL server user 'user_0d_2'
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 CREATE USER IF NOT EXISTS user_0d_2@localhost
 IDENTIFIED BY 'user_0d_2_pwd';
-GRANT ALL PRIVILEGES ON *.* TO user_0d_2@localhost;
+GRANT SELECT ON hbtn_0d_2.* TO user_0d_2@localhost;
