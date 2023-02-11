0ceeab38ffe089a3444beca497a92823d5b10ce0
HH <heather.hayes918@gmail.com>
2023-02-10 18:33:39 -0600

    Adding AS to name column

--- 0x00-SQL_introduction/14-average.sql
+++ 0x00-SQL_introduction/14-average.sql
@@ -1,2 +1,3 @@
 -- Compute the 'score' average of all records in table 'second_table'
-SELECT AVG(score) FROM second_table;
+SELECT AVG(score) AS average FROM second_table;
+-- 'AS' gives the column a name
