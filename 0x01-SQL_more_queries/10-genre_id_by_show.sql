16d785418350ed0a892375dbaccc949e3b2966d0
HH <heather.hayes918@gmail.com>
2023-02-12 16:43:38 -0600

    No join

--- 0x01-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x01-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,7 +1,7 @@
 -- import database from 'hbtn_0d_tvshows'
 -- Lists all shows in 'hbtn_0d_tvshows' with at least
 -- one genre linked
-SELECT tv_shows.title, tv_shows.id=tv_shows_genres.tv_show_id
-FROM tv_shows
-LEFT JOIN tv_shows_genres ON tv_shows.id=tv_shows_genres.tv_show_id
-GROUP BY tv_shows.title ASC, tv_shows_genres.genre_id ASC;
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows, tv_show_genres
+WHERE tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title, tv_show_genres.genre_id;
