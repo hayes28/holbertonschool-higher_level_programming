e89c6ba608ffe1d87e37adee298b1ba93ee0091c
HH <heather.hayes918@gmail.com>
2023-02-11 22:52:20 -0600

    Join

---
+++ 0x01-SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,6 @@
+-- Lists all cities contained in database 'hbtn_0d_usa'
+-- Each record should display: cities.id - cities.name - states.name
+-- Results must be sorted in ascending order by cities.id
+SELECT cities.id, cities.name, states.name
+  FROM cities
+  INNER JOIN states ON cities.state_id = states.id;
