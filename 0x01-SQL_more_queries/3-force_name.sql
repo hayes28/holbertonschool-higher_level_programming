05f022d1451e42822ab39ce18eaa4d5c7155096b
HH <heather.hayes918@gmail.com>
2023-02-11 21:47:01 -0600

    Added a comment

--- 0x01-SQL_more_queries/3-force_name.sql
+++ 0x01-SQL_more_queries/3-force_name.sql
@@ -1,4 +1,6 @@
 -- Create table 'force_name' with descriptions
+-- Had to change into 'hbtn_0d_2' database to use
+-- command in server. 'USE hbtn_0d_2;'
 CREATE TABLE IF NOT EXISTS force_name (
     id INT,
     name VARCHAR(256) NOT NULL
