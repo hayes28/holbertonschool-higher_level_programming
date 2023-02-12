7d697e545a695fc4734af13642eb15ed39c8ecb5
HH <heather.hayes918@gmail.com>
2023-02-12 17:05:49 -0600

    No genre

---
+++ 0x01-SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,7 @@
+-- list all shows contained in 'hbtn_0d_tvshows'
+-- without genres linked
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_show_genres
+RIGHT JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
+WHERE tv_show_genres.show_id IS NULL
+ORDER BY tv_shows.title, tv_show_genres.genre_id;
