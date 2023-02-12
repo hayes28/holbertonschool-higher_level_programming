b29dce737b11be6c989d99341536ca7e8e1b3298
HH <heather.hayes918@gmail.com>
2023-02-11 21:26:51 -0600

    Added comments

--- 0x01-SQL_more_queries/1-create_user.sql
+++ 0x01-SQL_more_queries/1-create_user.sql
@@ -1,4 +1,5 @@
 -- Create the MySQL server user 'user_0d_1'
+-- Had to make password 'User_0d_1_pwd' in server to satisfy system
 CREATE USER IF NOT EXISTS user_0d_1@localhost
 IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON *.* TO user_0d_1@localhost;
--- 0x01-SQL_more_queries/2-create_read_user.sql
+++ 0x01-SQL_more_queries/2-create_read_user.sql
@@ -1,4 +1,5 @@
 -- Create the database 'hbtn_0d_2' and the MySQL server user 'user_0d_2'
+-- Had to make password 'User_0d_2_pwd' in server to satisfy system
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 CREATE USER IF NOT EXISTS user_0d_2@localhost
 IDENTIFIED BY 'user_0d_2_pwd';
