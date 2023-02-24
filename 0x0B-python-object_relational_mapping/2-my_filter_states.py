ad9c57aa8b5effa18eced935040030d309468d2b
HH <heather.hayes918@gmail.com>
2023-02-24 17:40:40 -0600

    Print values and fixed task 1

--- 0x0B-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0B-python-object_relational_mapping/2-my_filter_states.py
@@ -1,24 +1,22 @@
 #!/usr/bin/python3
 """
-    Script that lists all states with a name starting with N
-    (upper N) from the database hbtn_0e_0_usa.
+Script that takes in an argument and displays
+all values in the states table of hbtn_0e_0_usa
+where name matches the argument.
 """
 import MySQLdb
 from sys import argv
 
 
-
-def print_stateN():
-    """
-    Print states from database
-    """
+def print_state_id():
     db = MySQLdb.connect(host="localhost",
                          port=3306,
                          user=argv[1],
                          passwd=argv[2],
                          database=argv[3])
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE '%N%'")
+    cur.execute("SELECT id FROM states WHERE name = %s", (argv[4],))
+
     for row in cur.fetchall():
         print(row)
 
@@ -27,4 +25,4 @@ def print_stateN():
 
 
 if __name__ == "__main__":
-    print_stateN()
+    print_state_id()
