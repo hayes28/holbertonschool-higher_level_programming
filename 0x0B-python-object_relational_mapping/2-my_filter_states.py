46f8809393aefa9d52ad379170aa0a9eaf0d1bc6
HH <heather.hayes918@gmail.com>
2023-02-25 16:47:58 -0600

    Got right output but tweeked for checker

--- 0x0B-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0B-python-object_relational_mapping/2-my_filter_states.py
@@ -9,14 +9,14 @@ import sys
 
 
 def print_state_id():
-    db = MySQLdb.connect(host="localhost",
+    db = MySQLdb.connect(host='localhost',
                          port=3306,
                          user=sys.argv[1],
                          passwd=sys.argv[2],
                          database=sys.argv[3])
     cur = db.cursor()
-    cur.execute("SELECT id FROM states WHERE name LIKE BINARY '{}'\
-                ORDER BY states.id ASC".format(sys.argv[4],))
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY '{}'\
+                ORDER BY states.id ASC".format(sys.argv[4]))
 
     for row in cur.fetchall():
         print(row)
