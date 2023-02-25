82d0e7694ea186d00d0bf0d7fa86bf19ab501bbb
HH <heather.hayes918@gmail.com>
2023-02-25 16:24:07 -0600

    Added sys.argv

--- 0x0B-python-object_relational_mapping/1-filter_states.py
+++ 0x0B-python-object_relational_mapping/1-filter_states.py
@@ -4,7 +4,7 @@
     (upper N) from the database hbtn_0e_0_usa.
 """
 import MySQLdb
-from sys import argv
+import sys
 
 
 
@@ -12,13 +12,14 @@ def print_stateN():
     """
     Print states from database
     """
-    db = MySQLdb.connect(host="localhost",
+    db = MySQLdb.connect(host='localhost',
                          port=3306,
-                         user=argv[1],
-                         passwd=argv[2],
-                         database=argv[3])
+                         user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         database=sys.argv[3])
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE '%N%'")
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%'\
+                ORDER BY states.id ASC;")
     for row in cur.fetchall():
         print(row)
 
