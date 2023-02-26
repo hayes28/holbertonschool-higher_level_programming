9276b5599e35210aa675b8874bf020da5f967573
HH <heather.hayes918@gmail.com>
2023-02-26 15:21:20 -0600

    Indentations fixed at .close

--- 0x0B-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0B-python-object_relational_mapping/4-cities_by_state.py
@@ -7,21 +7,28 @@ import MySQLdb
 import sys
 
 
-def print_cities():
+def print_cities_states():
+    """
+    Method to list all cities
+    """
     db = MySQLdb.connect(host='localhost',
                          port=3306,
                          user=sys.argv[1],
                          passwd=sys.argv[2],
                          database=sys.argv[3])
+
     cur = db.cursor()
-    cur.execute("SELECT cities.id, cities.name, states.name FROM cities\
-                LEFT JOIN states ON cities.state_id = states.id")
+    cur.execute("SELECT cities.id, cities.name, states.name\
+                    FROM cities, states\
+                    WHERE cities.state_id = states.id\
+                    ORDER BY cities.id")
+
     for row in cur.fetchall():
         print(row)
 
-        cur.close()
-        db.close()
+    cur.close()
+    db.close()
 
 
 if __name__ == "__main__":
-    print_cities()
+    print_cities_states()
