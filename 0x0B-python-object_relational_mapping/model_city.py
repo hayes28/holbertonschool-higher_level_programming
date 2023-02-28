b1b75851b16c941d97a6f4881f468ca9e08a521b
HH <heather.hayes918@gmail.com>
2023-02-27 21:14:16 -0600

    Last one

---
+++ 0x0B-python-object_relational_mapping/model_city.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Python file similar to model_state.py named model_city.py
+that contains the class definition of a City.
+"""
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class City(Base):
+    """
+    City class that inherits from Base
+    """
+    __tablename__ = 'cities'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('state.id'))
