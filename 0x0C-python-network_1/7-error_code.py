7d47158ab1622196f5bcd1360f1108fd9b9285d0
HH <heather.hayes918@gmail.com>
2023-03-09 21:18:05 -0600

    Task-7

---
+++ 0x0C-python-network_1/7-error_code.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Python script that takes in a URL,
+sends a request to the URL and
+displays the body of the response.
+"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    req = requests.get(sys.argv[1])
+    if req.status_code >= 400:
+        print("Error code: {}".format(req.status_code))
+    else:
+        print(req.text)
