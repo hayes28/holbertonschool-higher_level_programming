57b00ffb89b099d9004942e74e9f8ad7d39bf38a
HH <heather.hayes918@gmail.com>
2023-04-06 14:53:38 -0500

    Task4

---
+++ 0x00-javascript-warm_up/4-concat.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+// This script prints two arguments passed to it, in the following format: "is"
+// You must use console.log(...) to print all output
+// You are not allowed to use var
+
+const args = process.argv;
+console.log(args[2] + ' is ' + args[3]);
