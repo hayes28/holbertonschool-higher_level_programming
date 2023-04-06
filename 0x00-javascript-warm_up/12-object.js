e58d1c519f1728f03c542447d7227eb62f118835
HH <heather.hayes918@gmail.com>
2023-04-06 15:21:04 -0500

    Task12

---
+++ 0x00-javascript-warm_up/12-object.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+// This script replaces the value 12 with 89
+// You are not allowed to use var
+const myObject = {
+  type: 'object',
+  value: 12
+};
+console.log(myObject);
+myObject.value = 89;
+console.log(myObject);
