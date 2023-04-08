12113ac7861dfc9329d8d986ff5b63d98c746cf5
HH <heather.hayes918@gmail.com>
2023-04-08 15:45:32 -0500

    task6

---
+++ 0x01-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+// Class Square that defines a square and inherits from Square of 5-square.js
+// Must use the class notation for defining your class and extends
+// Create an instance method called charPrint(c) that prints the rectangle using the character c
+// If c is undefined, use the character X
+
+const Square5 = require('./5-square');
+class Square extends Square5 {
+  charPrint (c) {
+    if (c === undefined) {
+      c = 'X';
+    }
+    for (let i = 0; i < this.height; i++) {
+      console.log(c.repeat(this.width));
+    }
+  }
+}
+module.exports = Square;
