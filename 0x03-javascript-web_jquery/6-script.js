bf7bc723a466aaf162b129ee884a8791de7cc43f
HH <heather.hayes918@gmail.com>
2023-04-14 16:02:52 -0500

    Task6 & test file

---
+++ 0x03-javascript-web_jquery/6-script.js
@@ -0,0 +1,7 @@
+// JavaScript script that updates the text of the <header> element to New Header!!! when the user clicks on DIV#update_header
+// You can’t use document.querySelector to select the HTML tag
+// You must use the jQuery API
+
+$('#update_header').click(function () {
+  $('header').text('New Header!!!');
+});
